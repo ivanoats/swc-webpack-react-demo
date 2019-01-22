35e2a2592ef26573bf4946881ac9571d82269a50
Ivan Storck <ivan@ivanstorck.com>
2019-01-21 17:45:48 -0800

    tweak webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "./main.js"
+    filename: "main.js"
   },
   module: {
     rules: [
@@ -13,7 +13,24 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "swc-loader"
+          loader: "swc-loader",
+          options: {
+            jsc: {
+              parser: {
+                syntax: "ecmascript",
+                jsx: true
+              },
+              transform: {
+                react: {
+                  pragma: "React.createElement",
+                  pragmaFrag: "React.Fragment",
+                  throwIfNamespace: true,
+                  development: false,
+                  useBuiltins: false
+                }
+              }
+            }
+          }
         }
       }
     ]
